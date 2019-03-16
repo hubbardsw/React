0eed28336f5ae5b71b4862e45c9567525e9b919c
Scott Hubbard <swhubbard@gmail.com>
2019-03-15 21:38:00 -0700

    Added delete Ajax call with Axios

--- src/Components/Vehicles.jsx
+++ src/Components/Vehicles.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import axios from "axios";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash} from '@fortawesome/free-solid-svg-icons'
+library.add(faTrash)
 
 export default class Vehicles extends React.Component {
   state = {
@@ -8,8 +12,8 @@ export default class Vehicles extends React.Component {
 
   componentDidMount() {
     axios
-      .get("http://sabioapi2.azurewebsites.net/api/entities/vehicles", {
-        headers: { "sabio-auth": "UFV44SCCS" }
+      .get('http://sabioapi2.azurewebsites.net/api/entities/vehicles',
+       { headers: { "sabio-auth": "UFV44SCCS" }
       })
 
       .then(res => {
@@ -18,7 +22,11 @@ export default class Vehicles extends React.Component {
       });
   }
 
-  deleteVehicle = (index) => {
+  deleteVehicle = (id, index) => {
+    axios
+    .delete(`http://sabioapi2.azurewebsites.net/api/entities/vehicles/${id}`, {
+    headers: {"sabio-auth":"UFV44SCCS"}
+    })
     let vehicle = this.state.items
     vehicle.splice(index, 1)
     this.setState({items:vehicle})
@@ -27,18 +35,16 @@ export default class Vehicles extends React.Component {
   render(){
    const vehicleList = this.state.items.map((item, index) => {
       return(
-
+  
             <div key={item.id} className="vehicle-item" ref="vehicle">
               <div>Make:<span>{item.make}</span></div>
               <div>Model:<span>{item.model}</span></div>
               <div>Year:<span>{item.year}</span></div>
-              <button type="button" onClick={this.deleteVehicle.bind(this, index)}>Delete</button>
+              <button type="button"  className="btn-circle delete" onClick={ () => this.deleteVehicle(item.id, this, index)}><span><FontAwesomeIcon icon ="trash" /></span></button>
             </div>
             )  
 
     })
-  
-  
 
   return(
     <div>{vehicleList}</div>
